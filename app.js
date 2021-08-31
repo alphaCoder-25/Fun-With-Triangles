8816d6b35f206096774c3dbdb4d9e9bffa0ea8cd
anurudra555 <72567564+anurudra555@users.noreply.github.com>
2021-09-01 01:10:48 +0530

    some changes

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const angle1 = document.querySelector("#angle-1");
-const angle2 = document.querySelector("#angle-2");
-const angle3 = document.querySelector("#angle-3");
+const angles = document.querySelectorAll(".angles");
 const btnTriangle = document.querySelector("#btn-triangle");
 const output1 = document.querySelector("#output-1");
 
-btnTriangle.addEventListener("click", function checkTriangle() {
-    if( angle1.value + angle2.value + angle3.value === 180 ){
+console.log(angles);
+
+function checkTriangle(){
+
+    const sum = Number(angles[0].value) + Number(angles[1].value) + Number(angles[2].value);
+    if( sum === 180 ){
         output1.innerText = "Yes! This is a Triangle";
     } else {
         output1.innerText = "Nope! This is not a Triangle";
     }
-});
+}
+
+btnTriangle.addEventListener("click", checkTriangle);
