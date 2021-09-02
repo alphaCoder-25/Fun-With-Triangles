4c81f087f196dc03eb7e7fce141f044346e76207
anurudra555 <72567564+anurudra555@users.noreply.github.com>
2021-09-03 00:40:24 +0530

    implementing nav bar

--- hypotenuse.js
+++ hypotenuse.js
@@ -7,7 +7,7 @@ const output2 = document.querySelector("#output-2");
 function calculateHypotenuse(){
     const sum = (length1.value * length1.value) + (length2.value * length2.value);
     const hypotenuse = Math.sqrt(sum);
-    output2.innerText = "The Length of Hypotenuse is " + hypotenuse + " cm";
+    output2.innerText = "The length of hypotenuse is " + hypotenuse + " cm";
 }
 
 checkHypotenuse.addEventListener("click", calculateHypotenuse);
